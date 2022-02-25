3fd6032d96bc0e3204051f41b3bd2c44df3aadae
Amos Newswanger <aonews@gmail.com>
2022-02-25 03:51:23 +0100

    Add ddppo script and slurm script

--- job.sh
+++ job.sh
@@ -2,8 +2,8 @@
 #SBATCH -J test-hab
 #SBATCH --mail-type=ALL
 # Please make sure pathes are correct and directories exist beforehand::
-#SBATCH -e /home/an38gezy/cf-habitat/job_out/job5.err.%j
-#SBATCH -o /home/an38gezy/cf-habitat/job_out/job5.out.%j
+#SBATCH -e /home/an38gezy/thesis/cf-habitat/data/experiments/job_out/job.err.%j
+#SBATCH -o /home/an38gezy/thesis/cf-habitat/data/experiments/job_out/job.out.%j
 #                           instead of <Job_Name>, you can use %x (your Job_Name given above with '-J')
 #
 # CPU specification
@@ -11,10 +11,11 @@
 #SBATCH -c 24                 # 24 CPU cores per process 
 #                               can be referenced as $SLURM_CPUS_PER_TASK​ in the "payload" part
 #SBATCH --mem-per-cpu=3600    # Main memory in MByte per CPU core
-#SBATCH -t 00:15:00           # in hours:minutes, or '#SBATCH -t 10' - just minutes
+#SBATCH -t 00:10:00           # in hours:minutes, or '#SBATCH -t 10' - just minutes
 
 # GPU specification
-#SBATCH --gres=gpu:v100:1     # 2 GPUs of type NVidia "Volta 100"
+#SBATCH -C dgx
+#SBATCH --gres=gpu:a100:1     # 2 GPUs of type NVidia "Volta 100"
 
 # -------------------------------
 # your real job commands, eg.
@@ -23,7 +24,7 @@ module load gcc cuda
 nvidia-smi 1>&2
 
 
-cd /home/an38gezy/cf-habitat
+cd /home/an38gezy/thesis/cf-habitat
 
-export MAGNUM_GPU_VALIDATION=on
-python train.py
+# export MAGNUM_GPU_VALIDATION=on
+python main.py --run-type both --exp-config configs/experiments/crazyflie_baseline_rgb.yaml
